ea869e94c12bee2819fff776362f1b6121e9d535
hernandoagf <A7XHernando@gmail.com>
2022-02-16 15:14:25 -0300

    Add API key scope extension

--- plugin.rb
+++ plugin.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
-# version: 0.0.1
-# authors: Discourse
-# url: TODO
+# name: extended-api-scopes
+# about: A small Discourse plugin for extending API scopes
+# version: 0.1
+# authors: hernandoagf
+# url: https://github.com/hernandoagf/extended-api-scopes
 # required_version: 2.7.0
 # transpile_js: true
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :extended_api_scopes_enabled
 
 after_initialize do
+  add_api_key_scope(:logs, { index: { actions: %w[admin/staff_action_logs#index], params: %i[action_name] } })
 end
